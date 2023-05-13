0595f6b211d9565ba62a1ce8f0217809e7dff8f0
Walrusking <walruskinggaming@gmail.com>
2023-05-13 13:07:34 -0400

    Adds parameter test

---
+++ CSParser.UnitTests/ParameterTest.cs
@@ -0,0 +1,31 @@
+﻿namespace CSParser.UnitTests;
+
+public class ParameterTest
+{
+	private CSParameter _parameter;
+
+	[SetUp]
+	public void Setup()
+	{
+		_parameter = new CSParameter
+		{
+			Name = "test",
+			Type = "string"
+		};
+	}
+
+	[Test]
+	public void ParameterRenders()
+	{
+		Assert.That(_parameter.ToString(), Is.EqualTo("string test"));
+	}
+
+	[Test]
+	public void ParameterWithDefaultRenders()
+	{
+		_parameter.Optional = true;
+		_parameter.DefaultValue = "text";
+
+		Assert.That(_parameter.ToString(), Is.EqualTo("string test = \"text\""));
+	}
+}
